0aa45c7816e59db6afbdec8843156bc8b428450f
Bartosz <bartosz.gicala@gmail.com>
2023-01-20 20:38:26 +0100

    Composition concept

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import ExpItem from './components/ExpItem';
+
+import Exp from './components/Exp'
+
 function App() {
+	const expenses = [
+		{
+			id: 'e1',
+			title: 'Toilet paper',
+			amount: 93.35,
+			date: new Date(2023, 1, 12),
+		},
+		{
+			id: 'e2',
+			title: 'New TV',
+			amount: 258.65,
+			date: new Date(2023, 2, 3),
+		},
+		{
+			id: 'e3',
+			title: 'Car insurance',
+			amount: 589.83,
+			date: new Date(2023, 3, 17),
+		},
+		{
+			id: 'e4',
+			title: 'New desk',
+			amount: 234.56,
+			date: new Date(2023, 7, 24),
+		},
+	];
 	return (
 		<div>
-			<h2>Let's get started!</h2>
-			<ExpItem></ExpItem>
+			<Exp items={expenses} />
 		</div>
 	);
 }
--- src/components/ExpItem.js
+++ src/components/ExpItem.js
@@ -1,4 +1,18 @@
-function ExpItem() {
-	return <h2>Expense item!</h2>;
+
+import ExpDate from './ExpDate';
+import Card from './Card';
+import './ExpItem.css';
+
+function ExpItem(props) {
+	return (
+		<Card className='expense-item'>
+			<ExpDate date={props.date} />
+			<div className='expense-item__description'>
+				<h2>{props.title}</h2>
+				<div className='expense-item__price'>${props.amount}</div>
+			</div>
+		</Card>
+	);
 }
+
 export default ExpItem;
